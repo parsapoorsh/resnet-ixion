9c9a3d03e4892c6224065a15fff7328c0e7034b0
Parsa Poorshikhian <parsa.poorsh@gmail.com>
2025-03-09 01:16:55 +0330

    cleanup run_onnx.py

--- run_onnx.py
+++ run_onnx.py
@@ -1,4 +1,6 @@
+import warnings
 from pathlib import Path
+from typing import Mapping, List, Union
 
 import onnxruntime
 import numpy as np
@@ -11,35 +13,61 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
+default_providers = [
+    'CUDAExecutionProvider',
+    'MIGraphXExecutionProvider',
+    'ROCMExecutionProvider',
+    'OpenVINOExecutionProvider',
+    'CPUExecutionProvider',
+]
 
-def softmax(x):
+
+def softmax(x) -> np.ndarray:
     e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
     return e_x / np.sum(e_x, axis=1, keepdims=True)
 
 
+class OrientationDetection:
+    all_angles = [0, 90, 180, 270]  # classes
+
+    def __init__(self, model_path: Union[str, Path], providers: List[str] = None):
+        if not str(model_path).lower().endswith(".onnx"):
+            raise ValueError("model extention must be .onnx")
+        if providers is None:
+            providers = default_providers
+
+        with warnings.catch_warnings():
+            # UserWarning: Specified provider 'XXXXXXExecutionProvider' is not in available provider names.
+            # Available providers: 'TensorrtExecutionProvider, CUDAExecutionProvider, CPUExecutionProvider'
+            warnings.simplefilter("ignore", category=UserWarning)
+            self.ort_session = onnxruntime.InferenceSession(model_path, providers=providers)
+
+    @staticmethod
+    def read_image(image_path: str) -> Image.Image:
+        return Image.open(image_path)
+
+    @staticmethod
+    def to_array(image: Image.Image) -> np.ndarray:
+        image = image.convert("RGB")
+        numpy_image: np.ndarray = transform(image).unsqueeze(0).numpy()
+        return numpy_image
+
+    def get_angles(self, numpy_image: np.ndarray) -> Mapping[int, float]:
+        input_name = self.ort_session.get_inputs()[0].name
+        ort_inputs = {input_name: numpy_image}
+        ort_outs = self.ort_session.run(None, ort_inputs)
+        logits = ort_outs[0]
+        probabilities = softmax(logits)[0]
+        angles = {angle: score for angle, score in zip(self.all_angles, probabilities)}
+        return angles
+
+    def quick(self, image_path: str):
+        return self.get_angles(self.to_array(self.read_image(image_path)))
+
+
 def main(model_path: str, image_path: str):
-    providers = [
-        'CUDAExecutionProvider',
-        'MIGraphXExecutionProvider',
-        'ROCMExecutionProvider',
-        'OpenVINOExecutionProvider',
-        'CPUExecutionProvider',
-    ]
-    ort_session = onnxruntime.InferenceSession(model_path, providers=providers)
-    img = Image.open(image_path).convert("RGB")
-
-    tensor_image = transform(img).unsqueeze(0).numpy()
-
-    input_name = ort_session.get_inputs()[0].name
-    ort_inputs = {input_name: tensor_image}
-
-    ort_outs = ort_session.run(None, ort_inputs)
-    logits = ort_outs[0]
-
-    probabilities = softmax(logits)[0]
-
-    all_angles = [0, 90, 180, 270]
-    angles = {angle: score for angle, score in zip(all_angles, probabilities)}
+    od = OrientationDetection(model_path=model_path)
+    angles = od.quick(image_path)
 
     print("Orientation probabilities:")
     for angle, prob in angles.items():
